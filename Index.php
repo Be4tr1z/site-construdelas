23bfb006c649e9d51c6b6d1bb34ebc97c9bcafaa
Be4tr1z <33631035+Be4tr1z@users.noreply.github.com>
2021-10-02 18:11:48 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="CSS/estilo.css" type="text/css">
+</head>
+<body class="corpinho">
+    <h1>Bibi's site</h1>
+    <p>
+        hey, we dont have anything for you here yet.
+    </p>
+    <p>
+        Get out pls
+    </p>
+
+</body>
+</html>
